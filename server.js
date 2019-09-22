72baf72985af31d39368ed284b658035c3c851c2
Andrei Kosach <andreikosach@Andreis-MacBook-Pro.local>
2019-09-22 20:40:56 +0300

    Updated server port

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res)=> res.send('API running'));
 app.use('/api/users', require('./server/components/user/user.route'));
 app.use('/api/users/auth', require('./server/components/user/auth.route'));
 app.use('/api/tasks', require('./server/components/task/task.route'));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 /* eslint-disable */
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
