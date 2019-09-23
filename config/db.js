cf59478082fa0c45d46f4fe63311704de5de021e
Andrei Kosach <andreikosach@Andreis-MacBook-Pro.local>
2019-09-23 09:35:42 +0300

    Updated config; Dockerized app

--- config/db.js
+++ config/db.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+const db = config.get('db');
 
 const connectDB = async () => {
+    const username = process.env.DB_USERNAME || db.username;
+    const password = process.env.DB_PASSWORD || db.password;
+    const host     = process.env.DB_HOST     || db.host;
+    const database = process.env.DB_DATABASE || db.database;
+    const port     = process.env.DB_PORT     || db.port;
     try{
-        mongoose.connect(db, {
+        mongoose.connect(`mongodb://${username}:${password}@${host}:${port}/${database}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
